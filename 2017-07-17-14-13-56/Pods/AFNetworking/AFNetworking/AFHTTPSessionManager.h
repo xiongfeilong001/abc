39d604ddc7a5e5ecd613bf911371fa7f047047c2
XFL <XFL@163.com>
2017-07-17 16:05:51 +0800

    af

--- 2017-07-17-14-13-56/Pods/AFNetworking/AFNetworking/AFHTTPSessionManager.h
+++ 2017-07-17-14-13-56/Pods/AFNetworking/AFNetworking/AFHTTPSessionManager.h
@@ -1,6 +1,9 @@
 // AFHTTPSessionManager.h
 // Copyright (c) 2011–2015 Alamofire Software Foundation (http://alamofire.org/)
 //
+
+
+
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
